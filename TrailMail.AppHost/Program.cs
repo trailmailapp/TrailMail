28e33a961088c96115ddbdd47043bd8d9a0c808f
Qin Guan <qinguan20040914@gmail.com>
2024-11-18 13:06:03 +0000

    Add WebApp

--- TrailMail.AppHost/Program.cs
+++ TrailMail.AppHost/Program.cs
@@ -1,3 +1,9 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<TrailMail_WebApi>("api");
+var app = builder.AddNpmApp("app", "../TrailMail.WebApp", "dev")
+            .WithReference(api);
 
 builder.Build().Run();
